48cf253948d641f5e96c24932048cd1350e48a8f
CitRUS <citrus4009@gmail.com>
2020-03-20 23:45:19 +0300

    feat(app): Send messages from twitch to telegram

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,10 +1,73 @@
+import _ from "lodash"
+import TwitchChatReceiver, { IMessage } from "@tc2tc/twitch-chat-receiver"
+import TelegramSender from "@tc2tc/telegram-sender"
 import getConfig, { IConfig } from "./get-config"
 
 
-getConfig()
-    .then((config: Readonly<IConfig>) => {
+interface ITelegramConnection {
+    channel: string
+    botToken: string
+}
 
-        console.log(Object.keys(config))
+
+getConfig()
+    .then(async (config: Readonly<IConfig>) => {
+
+        const twitchChannels: Array<string> = _.map(config.channelPairs, "twitch")
+        const telegramChannels: Array<string> = _.map(config.channelPairs, "telegram")
+
+        const receiver: TwitchChatReceiver = new TwitchChatReceiver(...twitchChannels)
+        const sender: TelegramSender = new TelegramSender(config.telegramBotTokens, config.httpsProxy)
+
+        const inaccessible: Array<ITelegramConnection> = []
+
+        for (const telegramChannel of telegramChannels) {
+
+            const states: Array<boolean> = await sender.checkAccess(telegramChannel)
+
+            for (let i: number = 0; i < states.length; i++) {
+
+                if (!states[i]) {
+
+                    inaccessible.push({
+                        channel: telegramChannel,
+                        botToken: config.telegramBotTokens[i]
+                    })
+
+                }
+
+            }
+
+        }
+
+        if (inaccessible.length > 0) {
+
+            console.log("Bad connections:")
+
+            for (const tg of inaccessible) {
+
+                console.log(`  Bot: ${tg.botToken.slice(0, 9)} | Channel: ${tg.channel}`)
+
+            }
+
+            return
+
+        }
+
+        await receiver.launch()
+
+        receiver.onMessage((msg: IMessage) => {
+
+            const channel: string | undefined = _.find(config.channelPairs, { twitch: msg.channel })?.telegram
+
+            if (channel !== undefined) {
+
+                console.log(msg.text)
+                sender.send(channel, msg.text)
+
+            }
+
+        })
 
     })
     .catch((err: Error) => {
